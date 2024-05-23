c60521bff1362d3c8b5fdb61294fb9d4eb0b74cc
IsaacQH <isaacqwert@gmail.com>
2024-05-22 20:28:59 -0600

    Dandole el estilo

--- src/components/CriptoPriceDisplay.tsx
+++ src/components/CriptoPriceDisplay.tsx
@@ -7,23 +7,24 @@ export const CriptoPriceDisplay = () => {
     const hasResult = useMemo(() => !Object.values(result).includes(''), [result])  //Nos ayuda a revisar que existan valores para qe aparezca el componente
 
     return (
+        <div className="result-wrapper">
+  <h2>Cotización</h2>
+  {hasResult && (
+    <div className="cont">
+      <div className="result">
+        <img src={`https://cryptocompare.com/${result.IMAGEURL}`} alt="image crypto" />
         <div>
-            { hasResult && (
-                <>
-                    <h2>Cotización</h2>
-                    <div className="result">
-                        <img src={`https://cryptocompare.com/${result.IMAGEURL}`} alt="image crypto" />
-                        <div>
-                            <p>El precio es de:  <span>{result.PRICE}</span></p>
-                            <p>El precio es de:  <span>{result.HIGHDAY}</span></p>
-                            <p>El precio es de:  <span>{result.LOWDAY}</span></p>
-                            <p>El precio es de:  <span>{result.CHANGEPCT24HOUR}</span></p>
-                            <p>El precio es de:  <span>{result.LASTUPDATE}</span></p>
-                        </div>
-                    </div>
-                </>
-            )}
-
+          <p>El precio es de: <span>{result.PRICE}</span></p>
+          <p>Precio más alto del día: <span>{result.HIGHDAY}</span></p>
+          <p>Precio más bajo del día: <span>{result.LOWDAY}</span></p>
+          <p>Variación de las últimas 24 hrs: <span>{result.CHANGEPCT24HOUR}</span></p>
+          <p>Última actualización: <span>{result.LASTUPDATE}</span></p>
         </div>
+      </div>
+    </div>
+  )}
+</div>
+
+      
     )
 }
