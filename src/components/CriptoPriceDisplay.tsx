f44d506b7419c028ff1ac4611015a136209e2957
IsaacQH <isaacqwert@gmail.com>
2024-05-22 21:04:46 -0600

    Añadiendo el spinner

--- src/components/CriptoPriceDisplay.tsx
+++ src/components/CriptoPriceDisplay.tsx
@@ -1,30 +1,33 @@
 import { useCryptoStore } from "../store"
 import { useMemo } from "react"
+import { Spinner } from "./Spinner"
 
 export const CriptoPriceDisplay = () => {
 
     const result = useCryptoStore((state) => state.result)   //Abstraemos el resultado de
-    const hasResult = useMemo(() => !Object.values(result).includes(''), [result])  //Nos ayuda a revisar que existan valores para qe aparezca el componente
+    const loading = useCryptoStore((state) => state.loading)   //Abstraemos el resultado de
 
+    const hasResult = useMemo(() => !Object.values(result).includes(''), [result])  //Nos ayuda a revisar que existan valores para qe aparezca el componente
+    console.log(Object.values(result).includes(''), [result])
     return (
         <div className="result-wrapper">
-  <h2>Cotización</h2>
-  {hasResult && (
-    <div className="cont">
-      <div className="result">
-        <img src={`https://cryptocompare.com/${result.IMAGEURL}`} alt="image crypto" />
-        <div>
-          <p>El precio es de: <span>{result.PRICE}</span></p>
-          <p>Precio más alto del día: <span>{result.HIGHDAY}</span></p>
-          <p>Precio más bajo del día: <span>{result.LOWDAY}</span></p>
-          <p>Variación de las últimas 24 hrs: <span>{result.CHANGEPCT24HOUR}</span></p>
-          <p>Última actualización: <span>{result.LASTUPDATE}</span></p>
+        {loading ? <Spinner/> : hasResult && (
+            <>
+            <h2>Cotización</h2>
+            <div className="cont">
+            <div className="result">
+                <img src={`https://cryptocompare.com/${result.IMAGEURL}`} alt="image crypto" />
+                <div>
+                <p>El precio es de: <span>{result.PRICE}</span></p>
+                <p>Precio más alto del día: <span>{result.HIGHDAY}</span></p>
+                <p>Precio más bajo del día: <span>{result.LOWDAY}</span></p>
+                <p>Variación de las últimas 24 hrs: <span>{result.CHANGEPCT24HOUR}</span></p>
+                <p>Última actualización: <span>{result.LASTUPDATE}</span></p>
+                </div>
+            </div>
+            </div>
+            </>
+        )}
         </div>
-      </div>
-    </div>
-  )}
-</div>
-
-      
     )
 }
--- src/store.ts
+++ src/store.ts
@@ -1,40 +1,50 @@
+import { create } from 'zustand';
+import { CryptoPrice, Cryptocurrency, Pair } from './types';
+import { devtools } from 'zustand/middleware';
+import { fetchCurrentCryptoPrice, getCryptos } from './services/CryptoServices';
 
-import {create} from 'zustand'
-import { CryptoPrice, Cryptocurrency, Pair } from './types'
-import { devtools } from 'zustand/middleware'
-import { fetchCurrentCryptoPrice, getCryptos } from './services/CryptoServices'
-
-
-//Tipo de "dato" para realizar el schema para eluseCryptoStore
+// Tipo de "dato" para realizar el schema para el useCryptoStore
 export type CryptoStore = {
     cryptoCurrencies: Cryptocurrency[],
     result: CryptoPrice,
+    loading : boolean,
     fetchCryptos: () => Promise<void>,
-    fetchData: (pair:Pair) => Promise<void>
+    fetchData: (pair: Pair) => Promise<void>
 }
 
+// Store de los estados globales usando Zustand
+export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({     // Importando create, creamos el store de zustand con los estados
 
+    cryptoCurrencies: [], // State para guardar los cryptoCurrencies
 
+    result: {              // Se inicia con valores vacíos pero definidos
+        IMAGEURL: '',
+        PRICE: '',
+        HIGHDAY: '',
+        LOWDAY: '',
+        CHANGEPCT24HOUR: '',
+        LASTUPDATE: ''
+    } as CryptoPrice,
 
-//Store de los estados globales usando Zustand
-export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({     //Importando create, creamos el store de zustand con los estados
+    loading: false,
 
-    cryptoCurrencies:[], //State para guardat los cryptoCurrencies
-
-    result: {} as CryptoPrice,    //Se inicia en ceros y con el as se evita declarar cada propiedad vacia
-
-    fetchCryptos: async () => { //Funcion para hacer fecth en las cryptos
-        const cryptoCurrencies = await getCryptos()  //Llamamos a la funcion que obtiene los valores
+    fetchCryptos: async () => { // Función para hacer fetch en las cryptos
+        const cryptoCurrencies = await getCryptos();  // Llamamos a la función que obtiene los valores
         set(() => ({
-            cryptoCurrencies: cryptoCurrencies
-        }))
+            cryptoCurrencies
+        }));
     },
 
-    fetchData: async (pair) => {   //Función que recibe los precios y los pasa a el nuevo fetch
-        const result = await fetchCurrentCryptoPrice(pair)  //resultado de la abstraccion de los datos
+    fetchData: async (pair) => {   // Función que recibe los precios y los pasa al nuevo fetch
         set(() => ({
-            result : result   //Seteamos el valor de result a lo que obtuvimos
+            loading: true,   // Seteamos el valor de result a lo que obtuvimos
         }))
+        const result = await fetchCurrentCryptoPrice(pair);  // Resultado de la abstracción de los datos
+        setTimeout(() => {
+            set(() => ({
+                result,   // Establecemos el resultado obtenido
+                loading: false, // Cambiamos loading a false después del retraso
+            }));
+        }, 1000); // 1000 milisegundos = 1 segundo
     }
-    
-})))
+})));
